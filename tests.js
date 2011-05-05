4737248596c0d144f7c4dce63266b98b50ac6b10
eae@chromium.org <eae@chromium.org@48a18672-c925-a5f9-4b90-b3a8688770c7>
2011-05-05 22:54:52 +0000

    Initial checkin for test runner:   - Draft tests runner (bench.html)   - Draft tests definition file (tests.js)
    Initial set of benchmarks:
      - Adding columns to a table.
      - Adding rows to a table.
      - Descendant selectors.
      - Calling focus/select on input triggers layout.
      - Canvas drawing speed.
    
    
    git-svn-id: https://robohornet.googlecode.com/svn/trunk@2 48a18672-c925-a5f9-4b90-b3a8688770c7

---
+++ tests.js
@@ -0,0 +1,8 @@
+// Array of arrays, [filename, baseline, weight]
+var tests = [
+    ['addcol.html', 0.05, 0.5],
+    ['addrow.html', 0.005, 0.5],
+    ['descendantselector.html', 0.02, 1.0],
+    ['inputselect.html', 0.001, 0.75],
+    ['canvas_draw.html', 0.7, 1.0]
+];
