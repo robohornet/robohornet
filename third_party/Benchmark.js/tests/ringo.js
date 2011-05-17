64d10117383d15e57254f57f392430f834d16d6a
eae@chromium.org <eae@chromium.org@48a18672-c925-a5f9-4b90-b3a8688770c7>
2011-05-17 21:55:14 +0000

    - Changed scoring algorythm. - Imported Benchmark.js
    git-svn-id: https://robohornet.googlecode.com/svn/trunk@3 48a18672-c925-a5f9-4b90-b3a8688770c7

---
+++ third_party/Benchmark.js/tests/ringo.js
@@ -0,0 +1,41 @@
+(function() {
+
+  // load Benchmark
+  var Benchmark = require('../benchmark.js').Benchmark;
+
+  // check Benchmark.platform
+  print('Benchmark.platform: expected at least "RingoJS"; got "' +
+    Benchmark.platform + '";');
+
+  (function() {
+    global.arr = new Array(100);
+    var bench = new Benchmark(function() { arr.join(' '); }),
+        counter = 0;
+
+    // check synchronous run
+    bench.on('complete', function() {
+      print('Test sync: expected sync; got ' +
+        (counter ? 'a' : '') + 'sync ' + bench);
+
+      bench.removeAllListeners('complete');
+
+      // check asynchronous run
+      bench.on('complete', function() {
+        print('Test async: expected async; got ' +
+          (counter ? 'a' : '') + 'sync ' + bench);
+
+        // check Benchmark#MIN_TIME
+        print('Benchmark#MIN_TIME: expected at most 0.05; got ' +
+          Benchmark.prototype.MIN_TIME + ';');
+      });
+
+      counter = 0;
+      bench.run(true);
+      counter++;
+    });
+
+    bench.run();
+    counter++;
+  })();
+
+}());
