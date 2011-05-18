db23a349fc756b1e906c01b05254a88b7d11341e
komoroske@chromium.org <komoroske@chromium.org@48a18672-c925-a5f9-4b90-b3a8688770c7>
2011-05-18 16:56:04 +0000

    Replaced the stand-in weights with ones that add up to 100.
    git-svn-id: https://robohornet.googlecode.com/svn/trunk@4 48a18672-c925-a5f9-4b90-b3a8688770c7

--- tests.js
+++ tests.js
@@ -1,8 +1,9 @@
 // Array of arrays, [filename, baseline, weight]
+// The weight across all tests should sum to 100.
 var tests = [
-    ['addcol.html', 0.05, 0.5],
-    ['addrow.html', 0.005, 0.5],
-    ['descendantselector.html', 0.02, 1.0],
-    ['inputselect.html', 0.001, 0.75],
-    ['canvas_draw.html', 0.7, 1.0]
+    ['addcol.html', 0.05, 15.5],
+    ['addrow.html', 0.005, 15.5],
+    ['descendantselector.html', 0.02, 30.0],
+    ['inputselect.html', 0.001, 15.5],
+    ['canvas_draw.html', 0.7, 23.5]
 ];
