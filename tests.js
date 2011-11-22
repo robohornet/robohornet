69eecfb3b22c24e3a7327880229e3fd1b6d6bb9b
eae@google.com <eae@google.com@48a18672-c925-a5f9-4b90-b3a8688770c7>
2011-11-22 22:15:08 +0000

    Reimplemented test runner, test definitions and reworked UI.
    git-svn-id: https://robohornet.googlecode.com/svn/trunk@24 48a18672-c925-a5f9-4b90-b3a8688770c7

--- tests.js
+++ tests.js
@@ -7,3 +7,71 @@ var tests = [
     ['inputselect.html', 0.001, 15.5],
     ['canvas_draw.html', 0.7, 23.5]
 ];
+
+
+var benchmarks = [
+  {
+      file: 'addrow.html',
+      name: 'Add Rows to Table',
+      description: 'Tests adding rows to an existing table',
+      runs: [
+        ['10 rows',     10],
+        ['100 rows',   100],
+        ['1000 rows', 1000]
+      ],
+      weight: 5,
+      baselineScore: 10
+  },
+  
+  {
+      file: 'addcol.html',
+      name: 'Add Columns to Table',
+      description: 'Tests adding columns to an existing table',
+      runs: [
+        ['10 columns',     10],
+        ['100 columns',   100],
+        ['1000 columns', 1000]
+      ],
+      weight: 5,
+      baselineScore: 10
+  },
+
+  {
+      file: 'descendantselector.html',
+      name: 'Descendant Selector',
+      description: 'Tests descendant selectors at different DOM depths',
+      runs: [
+        ['10 nodes deep',     10],
+        ['100 nodes deep',   100],
+        ['1000 nodes deep', 1000]
+      ],
+      weight: 5,
+      baselineScore: 10
+  },
+
+  {
+      file: 'inputselect.html',
+      name: 'Input Selection',
+      description: 'Test the cost of selecting the text in an input field. In some browsers this causes a relayout which can be expensive.',
+      runs: [
+        ['10 nodes deep',     10],
+        ['100 nodes deep',   100],
+        ['1000 nodes deep', 1000]
+      ],
+      weight: 5,
+      baselineScore: 10
+  },
+
+  {
+      file: 'canvas_draw.html',
+      name: '2D Canvas Draw',
+      description: 'Test 2D canvas line painting.',
+      runs: [
+        ['100 lines', 100],
+        ['1000 lines', 1000],
+        ['10000 lines', 10000]
+      ],
+      weight: 5,
+      baselineScore: 10
+  }
+];
