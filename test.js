5870a1dfab592a1b6c5a85adba0f0e7d19c8c333
eae@google.com <eae@google.com@48a18672-c925-a5f9-4b90-b3a8688770c7>
2012-02-24 21:28:34 +0000

    - Fix table_scrolltop test to work in firefox. - Change harness for running a single test to use requestAnimationFrame to accurately account for layout time.
    git-svn-id: https://robohornet.googlecode.com/svn/trunk@94 48a18672-c925-a5f9-4b90-b3a8688770c7

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
+var requestAnimationFrameFunction = window.mozRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        window.webkitRequestAnimationFrame;
+
 if (window.opener && window.opener.__robohornet__ && window.location.search == '?use_test_runner') {
     document.body.appendChild(document.createTextNode('Running test using test runner...'));
     window.opener.__robohornet__.benchmarkLoaded();
@@ -9,8 +13,10 @@ if (window.opener && window.opener.__robohornet__ && window.location.search == '
     window.setTimeout(function() {
         var startTime = new Date().getTime();
         test();
-        var endTime = new Date().getTime();
-        document.body.appendChild(document.createTextNode('Ran test in ' + (endTime - startTime) + ' ms.'));
+        requestAnimationFrameFunction.call(window, function() {
+            var endTime = new Date().getTime();
+            document.body.appendChild(document.createTextNode('Ran test in ' + (endTime - startTime) + ' ms.'));
+        });
     }, 0);
 }
 
