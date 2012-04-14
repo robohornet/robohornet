e931629217acd5e9d233ca6a236a7e9ee5dee282
komoroske@google.com <komoroske@google.com@48a18672-c925-a5f9-4b90-b3a8688770c7>
2012-04-14 01:55:26 +0000

    Fixed a bug where people on the whitelist with Gmail accouns couldn't log in because the Users API returns those usernames without the @gmail.com. Also added some logging to log unsuccessful login attempts.
    git-svn-id: https://robohornet.googlecode.com/svn/trunk@142 48a18672-c925-a5f9-4b90-b3a8688770c7

--- simple-acl-server.py
+++ simple-acl-server.py
@@ -6,6 +6,7 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.api import users
 
 import re
+import logging
 
 FILE_TYPES = {
 	"css" : "text/css",
@@ -16,54 +17,58 @@ FILE_TYPES = {
 }
 
 ALLOWED_USERS = [
-    "lsmith@lucassmith.name",
-    "tom@tilde.io",
-    "john.david.dalton@gmail.com",
-    "ariya.hidayat@gmail.com",
-    "joestagner@gmail.com",
-    "wycats@gmail.com",
-    "mathias@qiwi.be",
-    "rainypixels@gmail.com",
-    "elsigh@gmail.com",
-    "tlrobinson@gmail.com",
-    "ryan@wonko.com",
-    "allenjs@gmail.com",
-    "matthwk@gmail.com",
-    "*@google.com"
+	"lsmith@lucassmith.name",
+	"tom@tilde.io",
+	"john.david.dalton@gmail.com",
+	"ariya.hidayat@gmail.com",
+	"joestagner@gmail.com",
+	"wycats@gmail.com",
+	"mathias@qiwi.be",
+	"rainypixels@gmail.com",
+	"elsigh@gmail.com",
+	"tlrobinson@gmail.com",
+	"ryan@wonko.com",
+	"allenjs@gmail.com",
+	"matthwk@gmail.com",
+	"*@google.com"
 ]
 
 ALLOWED_USERS = [re.compile("^" + pattern.replace(".", "\.").replace("*", ".*") + "$", re.IGNORECASE) for pattern in ALLOWED_USERS]
 
 class ACLPage(webapp.RequestHandler):
-    def get(self, path):
-    	user = str(users.get_current_user())
-    	#Check if the user is allowed.
-    	if not any(pattern.match(user) for pattern in ALLOWED_USERS):
-    		self.response.set_status(404)
-    		self.response.out.write(webapp.Response.http_status_message(404))
-    		return
-    	#Remove the leading /
-    	path = path[1:]
-    	if not path:
-    		path = "robohornet.html"
-    	file_type = path.split(".")[-1]
-    	if file_type not in FILE_TYPES:
-    		self.response.set_status(500)
-    		self.response.out.write("Disallowed file type")
-    		return
-    	try:
-    		f = open(path)
-    	except IOError:
-    		self.response.set_status(404)
-    		self.response.out.write(webapp.Response.http_status_message(404))
-    		return
-        self.response.headers['Content-Type'] = FILE_TYPES[file_type]
-        self.response.out.write(f.read())
+	def get(self, path):
+		user = str(users.get_current_user())
+		#App engine reports gmail addresses without the "gmail.com"
+		if "@" not in user:
+			user = user + "@gmail.com"
+		#Check if the user is allowed.
+		if not any(pattern.match(user) for pattern in ALLOWED_USERS):
+			self.response.set_status(404)
+			logging.warning("|%s| tried to log in but was blacklisted" % user)
+			self.response.out.write(webapp.Response.http_status_message(404))
+			return
+		#Remove the leading /
+		path = path[1:]
+		if not path:
+			path = "robohornet.html"
+		file_type = path.split(".")[-1]
+		if file_type not in FILE_TYPES:
+			self.response.set_status(500)
+			self.response.out.write("Disallowed file type")
+			return
+		try:
+			f = open(path)
+		except IOError:
+			self.response.set_status(404)
+			self.response.out.write(webapp.Response.http_status_message(404))
+			return
+		self.response.headers['Content-Type'] = FILE_TYPES[file_type]
+		self.response.out.write(f.read())
 
 application = webapp.WSGIApplication([('(.*)', ACLPage)], debug = True)
 
 def main():
-    run_wsgi_app(application)
+	run_wsgi_app(application)
 
 if __name__ == "__main__":
-    main()
+	main()
